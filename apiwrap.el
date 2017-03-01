9434bad19c8997dfb172fc7713ce2bec922deb9b
Sean Allred <code@seanallred.com>
2017-02-28 23:36:43 -0600

    Simplify expansion considerably

--- apiwrap.el
+++ apiwrap.el
@@ -78,40 +78,33 @@ Example:
 
 (defun apiwrap-plist->alist (plist)
   "Convert PLIST to an alist.
-Alist keys will be symbols and its values will be coerced into
-strings."
+If a PLIST key is a `:keyword', then it is converted into a
+symbol `keyword'."
   (when (= 1 (mod (length plist) 2))
     (error "bad plist"))
-  (apiwrap--plist->alist-internal plist nil))
-
-(defun apiwrap--plist->alist-internal (plist alist-build)
-  "Recursively build ALIST-BUILD from PLIST. "
-  (if plist (cons (let ((key (car plist))
-                        (val (cadr plist)))
-                    (cons (apiwrap--kw->sym key) val))
-                  (apiwrap--plist->alist-internal (cddr plist) alist-build))))
+  (let (alist)
+    (while plist
+      (push (cons (apiwrap--kw->sym (car plist))
+                  (cadr plist))
+            alist)
+      (setq plist (cddr plist)))
+    alist))
 
 (defun apiwrap--kw->sym (kw)
   "Convert a keyword to a symbol."
-  (intern (substring (symbol-name kw) 1)))
+  (if (keywordp kw)
+      (intern (substring (symbol-name kw) 1))
+    kw))
 
-(defun apiwrap--get (prop default override)
-  "Get PROP by merging DEFAULT and OVERRIDE.
-If OVERRIDE as a value for PROP, that value is returned (even if
-that value is nil)."
-  (if (memq prop override)
-      (plist-get override prop)
-    (plist-get default prop)))
-
-(defun apiwrap--defresource-doc (doc object-param-doc method external-resource link)
+(defun apiwrap--docfn (doc object-param-doc method external-resource link)
   "Documentation string for resource-wrapping functions created
 by `apiwrap--defresource'"
   (format "%s
 
-%sPARAMS is a plist of parameters appended to the method call.
-
-DATA is a data structure to be sent with this request.  If it's
+%sDATA is a data structure to be sent with this request.  If it's
 not required, it can simply be omitted.
+
+PARAMS is a plist of parameters appended to the method call.
 
 %s
 
@@ -129,139 +122,176 @@ which is documented at
           (upcase (symbol-name method))
           external-resource link))
 
-(defun apiwrap--defmethod-doc (service-name method)
+(defun apiwrap--docmacro (service-name method)
   "Documentation string for macros created by
 `apiwrap-new-backend'"
   (apply #'format "Define a new %s resource wrapper function.
 
 RESOURCE is the API endpoint as written in the %s API
-documentation.  This string will be used to create the symbol for
-the new function.
+documentation.  Along with the backend prefix (from
+`apiwrap-new-backend') and the method (%s), this string will be
+used to create the symbol for the new function.
 
-DOC is a documentation string for the new function.  Usually,
-this can be copied from the %s API documentation.
+DOC is a specific documentation string for the new function.
+Usually, this can be copied from the %s API documentation.
 
-VERSION is the %s API version this resource is from.  It is
-passed to LINK-FUNC as the first argument.
+VERSION is the %s API version this resource is from.
 
-LINK is a link to the %s API documentation.  It is passed to
-LINK-FUNC as the second argument.
+LINK is a link to the %s API documentation.
 
 If non-nil, OBJECT is a symbol that will be used to resolve
 parameters in the resource and will be a required argument of the
-new function.  If nil, it is ignored.  For details on this
-behavior, see `apiwrap-resolve-api-params'.
+new function.  Its documentation (from the standard parameters of
+the call to `apiwrap-new-backend') will be inserted into the
+docstring of the generated function.
 
 If non-nil, INTERNAL-RESOURCE is the resource-string used to
-resolve OBJECT to the ultimate call.
-
-KWARGS is a list of override parameters.  Values set
-here (notably those explicitly set to nil) will take precedence
-of the defaults provided to `apiwrap-new-backend'."
-         (upcase (symbol-name method))
-         (make-list 4 service-name)))
-
-(defun apiwrap--defresource (prefix method api-func link-func
-                                    standard-parameters default-kwargs kwargs
-                                    external-resource doc version link
-                                    object internal-resource)
-  "Define a new resource-wrapping function.
-
-PREFIX, METHOD, and EXTERNAL-RESOURCE will be combined to form
-the function name.  METHOD is one of `get', `put', `head',
-`post', `patch', or `delete' and EXTERNAL-RESOURCE should be the
-calling syntax as advertised by the API.  This naming scheme is
-intended to make it easy for application developers to find your
-wrappers and should be followed, but you can consider aliasing
-the generated function name to a different name if you so
-choose.
-
-API-FUNC is the primitive function of your API that corresponds
-to METHOD.  See `apiwrap-new-backend' for details.
-
-LINK-FUNC, see `apiwrap-new-backend'.
-
-STANDARD-PARAMETERS, see `apiwrap-new-backend'.
-
-DEFAULT-KWARGS, see `apiwrap-new-backend'.
-
-KWARGS overrides DEFAULT-KWARGS (notably those set to nil).
-
-DOC is a documentation string for this resource.
-
-VERSION is the API version this resource is from.  It is passed
-to LINK-FUNC as the first argument.
-
-LINK is a link to the API documentation.  It is passed
-to LINK-FUNC as the second argument.
-
-OBJECT is a symbol that will be used to resolve dynamic
-references in the resource-string.  Its documentation will be
-inserted into the docstring the the new function.  When the
-function is called, the parameter's value will be used to build
-the actual call to the API.
-
-INTERNAL-RESOURCE, if different from EXTERNAL-RESOURCE, will be
-used to resolve OBJECT instead.  This is useful in the likely
-event that the advertised resource syntax does not align with the
-structure of the object it works with.  For example, GitHub's
+resolve OBJECT to the ultimate call instead of RESOURCE.  This is
+useful in the likely event that the advertised resource syntax
+does not align with the structure of the object it works with.
+For example, GitHub's endpoint
 
     GET /repos/:owner/:repo/issues resource
 
-would be created thusly
+would be written as
 
     \(prefix-defget \"/repos/:owner/:repo/issues\"
       \"List issues for a repository.\"
       3 \"issues/#list-issues-for-a-repository\"
       repo \"/repos/:owner.login/:name/issues\"\)
 
-defining a function called `prefix-get-repos-owner-repo-issues'
+defining a function called `<prefix>-get-repos-owner-repo-issues'
 and taking an object with the structure
 
     \(\(owner \(login . \"octocat\"\)\)
      \(name . \"hello-world\"\)
 
 See the documentation of `apiwrap-resolve-api-params' for more
-details on that behavior."
-  (declare (indent defun))
-  (let* ((internal-resource (or internal-resource external-resource))
-         (symbol external-resource)
-         (symbol (replace-regexp-in-string "/" "-" symbol t t))
-         (symbol (replace-regexp-in-string ":" ""  symbol t t))
-         (symbol (intern (concat prefix "-" (symbol-name method) symbol)))
-         (args (append (when object (list object)) '(&optional data &rest params)))
-         (object-param-doc (alist-get object standard-parameters))
-         (link (funcall link-func version link))
-         (form `(apply ',api-func
-                       ,(apiwrap-resolve-api-params object internal-resource)
+details on that behavior.
+
+FUNCTIONS is a list of override configuration parameters.  Values
+set here (notably those explicitly set to nil) will take
+precedence over the defaults provided to `apiwrap-new-backend'."
+         (upcase (symbol-name method))
+         service-name
+         (upcase (symbol-name method))
+         (make-list 3 service-name)))
+
+(defun apiwrap-genfunsym (prefix api-method &optional resource)
+  "Generate a symbol for a macro/function."
+  (let ((api-method (symbol-name (apiwrap--kw->sym api-method))))
+    (intern
+     (if resource
+         (format "%s-%s%s" prefix api-method
+                 (->> resource
+                      (replace-regexp-in-string "/" "-")
+                      (replace-regexp-in-string ":" "")))
+       (format "defapi%s-%s" api-method prefix)))))
+
+(defun apiwrap-stdgenlink (sym)
+  "Standard link generation function.
+Returns the `api-documentation' property of SYM."
+  (plist-get (symbol-plist sym) 'apiwrap-documentation))
+
+(defconst apiwrap-primitives
+  '(get put head post patch delete)
+  "List of primitive methods.  These are required to be
+configured.")
+
+(defun apiwrap-genmacros (name prefix standard-parameters functions)
+  "Validate arguments and generate all macro forms"
+  ;; Default to raw link entered in the macro
+  (add-to-list 'functions '(link . #'apiwrap-stdgenlink) t)
+
+  ;; Verify all extension functions are actually functions
+  (dolist (f functions)
+    (let ((key (car f)) (fn (cdr f)))
+      (unless (or (functionp fn) (and (consp fn)
+                                      (eq 'function (car fn))
+                                      (functionp (cadr fn))))
+        (if (memq key apiwrap-primitives)
+            (error "Primitive function literal required: %s" key)
+          (byte-compile-warn "Unknown function for `%S': %S" key fn)))))
+
+  ;; Build the macros
+  (let (super-form macro-form primitive-func)
+    (dolist (primitive (reverse apiwrap-primitives))
+      (setq macro-form nil
+            primitive-func (alist-get primitive functions))
+      (let ((macrosym (apiwrap-genfunsym prefix primitive)))
+        (push `(defmacro ,macrosym (resource doc version link
+                                             &optional object internal-resource
+                                             &rest functions)
+                 ,(apiwrap--docmacro name (apiwrap--kw->sym primitive))
+                 (declare (indent defun) (doc-string 2))
+                 (apiwrap-gendefun ,name ,prefix ,standard-parameters ',primitive
+                                   resource doc version link object internal-resource
+                                   (append functions ',functions)))
+              super-form)))
+    super-form))
+
+(defun apiwrap-gendefun (name prefix standard-parameters method resource doc version link object internal-resource functions)
+  "Generate a single defun form"
+  (let ((internal-resource (or internal-resource resource))
+        (args '(&optional data &rest params))
+        (funsym (apiwrap-genfunsym prefix method resource))
+        fundoc resolved-resource form
+        primitive-func link-func post-process-func)
+
+    ;; Be smart about when configuration starts.  Neither `object' nor
+    ;; `internal-resource' can be keywords, so we know that if they
+    ;; are, then we need to shift things around.
+    (when (keywordp object)
+      (push internal-resource functions)
+      (push object functions)
+      (setq object nil internal-resource nil))
+    (when (keywordp internal-resource)
+      (push internal-resource functions)
+      (setq internal-resource nil))
+
+    ;; Now that our arguments have settled, let's use them
+    (when object (push object args))
+
+    (setq primitive-func (alist-get method functions)
+          post-process-func (alist-get 'post-process functions)
+          link-func (alist-get 'link functions))
+
+    ;; If our functions are already functions (and not quoted), we'll
+    ;; have to quote them for the actual defun
+    (when (functionp primitive-func)
+      (setq primitive-func `(function ,primitive-func)))
+    (when (functionp post-process-func)
+      (setq post-process-func `(function ,post-process-func)))
+    (when (functionp link-func)
+      (setq link-func `(function ,link-func)))
+
+    ;; Alright, we're ready to build our function
+    (setq resolved-resource (apiwrap-resolve-api-params object internal-resource)
+          form `(apply ,primitive-func ,resolved-resource
                        (if (keywordp data)
                            (list (apiwrap-plist->alist (cons data params)))
                          (list (apiwrap-plist->alist params) data))))
-         (post-process (apiwrap--get :post-process default-kwargs kwargs)))
-    (when (and object (not object-param-doc))
-      (user-error "Standard parameter `%s' not documented" object))
-    (when post-process
-      (if (functionp post-process)
-          (setq form (list post-process form))
-        (user-error "`%S' (:post-process value) is not a function" post-process)))
-    `(prog1
-         (defun ,symbol ,args ,(apiwrap--defresource-doc
-                                doc object-param-doc method
-                                external-resource link)
-                (declare (indent defun))
-                ,form)
-       ;; I feel like the following will be useful someday -- perhaps
-       ;; some sort of report on what API end-points are currently
-       ;; available
-       (put ',symbol 'apiwrap-prefix       ',(intern prefix))
-       (put ',symbol 'apiwrap-version       ,version)
-       (put ',symbol 'apiwrap-method       ',method)
-       (put ',symbol 'apiwrap-endpoint      ,external-resource)
-       (put ',symbol 'apiwrap-documentation ,link))))
 
-(defmacro apiwrap-new-backend (service-name prefix standard-parameters link-func
-                                            get-func put-func head-func post-func patch-func delete-func
-                                            &rest default-kwargs)
+    (when post-process-func
+      (setq form `(funcall ,post-process-func ,form)))
+
+    (put funsym 'apiwrap-prefix        prefix)
+    (put funsym 'apiwrap-version       version)
+    (put funsym 'apiwrap-method        method)
+    (put funsym 'apiwrap-endpoint      resource)
+    (put funsym 'apiwrap-documentation link)
+
+    (setq fundoc (apiwrap--docfn doc (alist-get object standard-parameters) method resource
+                                 (funcall (cadr (alist-get 'link functions)) funsym)))
+
+    `(prog1 (defun ,funsym ,args ,fundoc ,form)
+       (put ',funsym 'apiwrap-prefix        ,prefix)
+       (put ',funsym 'apiwrap-version       ,version)
+       (put ',funsym 'apiwrap-method       ',method)
+       (put ',funsym 'apiwrap-endpoint      ,resource)
+       (put ',funsym 'apiwrap-documentation ,link))))
+
+(defmacro apiwrap-new-backend (name prefix standard-parameters &rest functions)
   "Define a new API backend.
 
 SERVICE-NAME is the name of the service this backend will wrap.
@@ -281,48 +311,38 @@ a version (number) and a link (string) and returns a fully
 qualified URL that links to the official documentation of the
 resource.
 
-GET-FUNC, PUT-FUNC, HEAD-FUNC, POST-FUNC, PATCH-FUNC, and
-DELETE-FUNC are the primitive method-functions of your API.  See
-package `ghub' as an example of the kinds of primitives these
-macros are design for; you may wish to consider writing wrappers.
-Each function is expected to take a resource-string as the first
-parameter.  The second parameter should be an alist of parameters
-to the resource.  The third parameter should be an alist of data
-for the resource (e.g., for posting).
+FUNCTIONS is a list of arguments to configure the generated
+macros.
 
-DEFAULT-KWARGS is a list of additional arguments to configure the
-generated macros.
+  Required:
 
-  :post-process   function to process the responses of the API
-                  before returning"
-  (declare (indent defun))
-  (when (memq nil (mapcar #'functionp (list get-func put-func head-func post-func patch-func delete-func)))
-    (byte-compile-warn "one or more API primitives for %S service not known to exist" service-name))
-  (unless (functionp link-func)
-    (error "invalid link function"))
+    :get :put :head :post :patch :delete
 
-  `(prog1 ,service-name
-     ,@(mapcar (lambda (cell)
-                 (let* ((method (car cell)) (func (cdr cell))
-                        (symbol (intern (concat prefix "-def" (symbol-name method)))))
-                   `(defmacro ,symbol (resource doc version link
-                                                &optional object internal-resource
-                                                &rest kwargs)
-                      ,(apiwrap--defmethod-doc service-name method)
-                      (declare (indent defun) (doc-string 2))
-                      (when (keywordp object)
-                        (setq kwargs (cons object (cons internal-resource kwargs))
-                              object nil
-                              internal-resource nil))
-                      (when (keywordp internal-resource)
-                        (setq kwargs (cons internal-resource kwargs)
-                              internal-resource nil))
-                      (apiwrap--defresource ,prefix ',method ',func
-                        ,link-func ',standard-parameters ',default-kwargs kwargs
-                        resource doc version link
-                        object internal-resource))))
-               `((get . ,get-func) (put . ,put-func) (head . ,head-func)
-                 (post . ,post-func) (patch . ,patch-func) (delete . ,delete-func)))))
+        API primitives.  See package `ghub' as an example of the
+        kinds of primitives these macros are design for; you may
+        wish to consider writing wrappers.  Each function is
+        expected to take a resource-string as the first
+        parameter.  The second parameter should be an alist of
+        parameters to the resource.  The third parameter should
+        be an alist of data for the resource (e.g., for posting).
+
+  Optional:
+
+    :link
+
+        Function to process a symbol and return a link.  Should
+        take a symbol as its sole parameter and return a
+        fully-qualified URL to be considered the official
+        documentation of the API endpoint.  The default is to
+        return the raw LINK passed to `defapi<method>-<prefix>'.
+
+    :post-process
+
+        Function to process the responses of the API before
+        returning.
+"
+  `(prog1 ,name ,@(apiwrap-genmacros name prefix standard-parameters
+                                     (apiwrap-plist->alist functions))))
 
 (provide 'apiwrap)
 ;;; apiwrap.el ends here
