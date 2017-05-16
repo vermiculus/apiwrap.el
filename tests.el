16f7b2c3c0e2568bc302f481f25b16df2b0260d7
Sean Allred <code@seanallred.com>
2017-05-15 20:57:26 -0500

    Declare tests

--- tests.el
+++ tests.el
@@ -13,26 +13,30 @@
                      :post #'ghub-post :patch #'ghub-patch :delete #'ghub-delete
                      :link #'apiwrap-test--gen-link)
 
-(defapiget-my-github-wrapper "/repos/:owner/:repo/issues"
-  "List issues for a repository."
-  "issues/#list-issues-for-a-repository"
-  (repo) "/repos/:repo.owner.login/:repo.name/issues")
+(ert-deftest apiwrap-macros ()
+  (should (equal 'my-github-wrapper-get-repos-owner-repo-issues
+                 (defapiget-my-github-wrapper "/repos/:owner/:repo/issues"
+                   "List issues for a repository."
+                   "issues/#list-issues-for-a-repository"
+                   (repo) "/repos/:repo.owner.login/:repo.name/issues")))
 
-(defapiget-my-github-wrapper "/repos/:owner/:repo/issues/:number"
-  "List issues for a repository."
-  "issues/#list-issues-for-a-repository"
-  (repo issue) "/repos/:repo.owner.login/:repo.name/issues/:issue.number")
+  (should (equal 'my-github-wrapper-get-repos-owner-repo-issues-number
+                 (defapiget-my-github-wrapper "/repos/:owner/:repo/issues/:number"
+                   "List issues for a repository."
+                   "issues/#list-issues-for-a-repository"
+                   (repo issue) "/repos/:repo.owner.login/:repo.name/issues/:issue.number")))
 
-(defapiget-my-github-wrapper "/repos/:owner/:repo/issues/:number/comments"
-  "List comments on an issue."
-  "issues/comments/#list-comments-on-an-issue"
-  (repo issue) "/repos/:repo.owner.login/:repo.name/issues/:issue.number/comments")
+  (should (equal 'my-github-wrapper-get-repos-owner-repo-issues-number-comments
+                 (defapiget-my-github-wrapper "/repos/:owner/:repo/issues/:number/comments"
+                   "List comments on an issue."
+                   "issues/comments/#list-comments-on-an-issue"
+                   (repo issue) "/repos/:repo.owner.login/:repo.name/issues/:issue.number/comments"))))
 
-(let* ((my-repo '((owner (login . "vermiculus"))
-                  (name . "apiwrap.el")))
-       (my-issue (my-github-wrapper-get-repos-owner-repo-issues-number my-repo '((number . 1))))
-       (all-issues (my-github-wrapper-get-repos-owner-repo-issues my-repo :state "all")))
-  (list
-   (my-github-wrapper-get-repos-owner-repo-issues-number-comments my-repo my-issue)
-   (= (alist-get 'id my-issue)
-      (alist-get 'id (car all-issues)))))
+(ert-deftest apiwrap-usage ()
+  (let* ((my-repo '((owner (login . "vermiculus"))
+                    (name . "apiwrap.el")))
+         (my-issue (my-github-wrapper-get-repos-owner-repo-issues-number my-repo '((number . 1))))
+         (all-issues (my-github-wrapper-get-repos-owner-repo-issues my-repo :state "all")))
+    (should (my-github-wrapper-get-repos-owner-repo-issues-number-comments my-repo my-issue))
+    (should (= (alist-get 'id my-issue)
+               (alist-get 'id (car all-issues))))))
