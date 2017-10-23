3285b5812dc38dbfd44d425459ec18db975755ee
Sean Allred <code@seanallred.com>
2017-10-22 23:36:38 -0500

    Add :around macro argument
    Use this keyword argument to wrap the entire request form using macro.
    Useful for caching requests, among other things.

--- apiwrap.el
+++ apiwrap.el
@@ -230,9 +230,12 @@ appropriately handle all of these symbols as a METHOD.")
   ;; Verify all extension functions are actually functions
   (dolist (f functions)
     (let ((key (car f)) (fn (cdr f)))
-      (unless (or (functionp fn) (and (consp fn)
-                                      (eq 'function (car fn))
-                                      (functionp (cadr fn))))
+      (unless (or (functionp fn)
+                  (macrop fn)
+                  (and (consp fn)
+                       (eq 'function (car fn))
+                       (or (functionp (cadr fn))
+                           (macrop (cadr fn)))))
         (if (memq key apiwrap-primitives)
             (error "Primitive function literal required: %s" key)
           (byte-compile-warn "Unknown function for `%S': %S" key fn)))))
@@ -264,7 +267,7 @@ Otherwise, just return VALUE quoted."
         (funsym (apiwrap-gensym prefix method resource))
         resolved-resource-form form functions
         data-massage-func params-massage-func
-        primitive-func link-func)
+        primitive-func link-func around)
 
     ;; Be smart about when configuration starts.  Neither `objects' nor
     ;; `internal-resource' can be keywords, so we know that if they
@@ -283,6 +286,7 @@ Otherwise, just return VALUE quoted."
     (when objects (setq args (append objects args)))
 
     (setq internal-resource (or internal-resource resource)
+          around (alist-get 'around functions)
           primitive-func (alist-get 'request functions)
           data-massage-func (alist-get 'pre-process-data functions)
           params-massage-func (alist-get 'pre-process-params functions)
@@ -308,6 +312,11 @@ Otherwise, just return VALUE quoted."
                       (list ,(apiwrap--maybe-apply params-massage-func '(cons data params)) nil)
                     (list ,(apiwrap--maybe-apply params-massage-func 'params)
                           ,(apiwrap--maybe-apply data-massage-func 'data)))))
+
+    (when around
+      (unless (macrop around)
+        (error ":around must be a macro: %S" around))
+      (setq form (macroexpand `(,around ,form))))
 
     (let ((props `((prefix   . ,prefix)
                    (method   . ,method)
@@ -392,7 +401,12 @@ macros.
     :pre-process-data
 
         Function to process request data before the request is
-        passed to the `:request' function."
+        passed to the `:request' function.
+
+    :around
+
+        Macro to wrap around the request form (which is passed as
+        the only argument)."
   (declare (indent 2))
   (let ((sname (cl-gensym)) (sprefix (cl-gensym))
         (sstdp (cl-gensym)) (sfuncs (cl-gensym)))
